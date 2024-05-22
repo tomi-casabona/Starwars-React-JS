02c3454a6c4e1fd9745cf71234220b9f651c2f6a
tomi-casabona <tomas.casabona@gmail.com>
2024-05-22 11:07:27 +0200

    added Provider and Store into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import './assets/fonts.css';
+import "./assets/fonts.css";
 import "./assets/App.css";
 import { auth } from "./credenciales.js";
 import { onAuthStateChanged } from "firebase/auth";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header.jsx";
 import { AppRoutes } from "./helpers/routes.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
@@ -25,14 +27,14 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
-      <div className="bg-body-img bg-cover bg-no-repeat min-h-screen font-kanit">
-        <Header />
-        <AppRoutes isLogged={isLogged} />
-      </div>
+        <div className="bg-body-img bg-cover bg-no-repeat min-h-screen font-kanit">
+          <Header />
+          <AppRoutes isLogged={isLogged} />
+        </div>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
