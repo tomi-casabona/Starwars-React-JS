02035744441c991f2201656b103f4622ef036647
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-05-27 12:59:36 +0200

    fixed route issues and home navigate by default

--- src/helpers/routes.jsx
+++ src/helpers/routes.jsx
@@ -1,17 +1,18 @@
-import { Route, Routes } from "react-router-dom";
-import { Home } from "../components/pages/Home";
-import { Login } from "../components/pages/Login";
-import { SignIn } from "../components/pages/SignIn";
-import { ProtectedRoute } from "../utils/ProtectedRoute";
-import { StarShips } from "../components/pages/StarShips";
-import { StarShipDetail } from "../components/shipsComponents/StarshipDetail";
-import { useSelector } from "react-redux";
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../components/pages/Home';
+import { Login } from '../components/pages/Login';
+import { SignIn } from '../components/pages/SignIn';
+import { ProtectedRoute } from '../utils/ProtectedRoute';
+import { StarShips } from '../components/pages/StarShips';
+import { StarShipDetail } from '../components/shipsComponents/StarshipDetail';
+import { useSelector } from 'react-redux';
 
 export const AppRoutes = () => {
   const isLogged = useSelector((state) => state.user.isLogged);
 
   return (
     <Routes>
+      <Route path="/*" element={<Navigate to="/" />}></Route>
       <Route path="/" element={<Home />} />
       <Route path="/logIn" element={<Login />} />
       <Route path="/signIn" element={<SignIn />} />
