e845e0132a78819af24b549a64fb240b1d57764e
Tomas <tomas.casabona@gmail.com>
2024-05-27 16:05:58 +0200

    first test with redux done. it's only a proof

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,5 +1,7 @@
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+import '@testing-library/jest-dom';
+
 
 afterEach(()=>{ //depues de cada prueba
     cleanup();//limpia el dom
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { configDefaults } from 'vitest/config';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.js"
   }
