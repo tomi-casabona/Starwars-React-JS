76184fe7a02960f6e2d62652fcba8df9d09c0c0b
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-05-28 19:30:39 +0200

    Films Component render test

--- src/components/filmsComponents/Films.test.jsx
+++ src/components/filmsComponents/Films.test.jsx
@@ -10,13 +10,38 @@ const starshipFilms = [
 ];
 
 describe("Films", () => {
-  let mockFn = vi.fn();
+  const starship = {
+    "name": "Millennium Falcon",
+    "model": "YT-1300 light freighter",
+    "manufacturer": "Corellian Engineering Corporation",
+    "cost_in_credits": "100000",
+    "length": "34.37",
+    "max_atmosphering_speed": "1050",
+    "crew": "4",
+    "passengers": "6",
+    "cargo_capacity": "100000",
+    "consumables": "2 months",
+    "hyperdrive_rating": "0.5",
+    "MGLT": "75",
+    "starship_class": "Light freighter",
+    "pilots": [
+      "https://swapi.dev/api/people/13/",
+      "https://swapi.dev/api/people/14/",
+      "https://swapi.dev/api/people/25/",
+      "https://swapi.dev/api/people/31/"
+    ],
+    "films": [
+      "https://swapi.dev/api/films/1/",
+      "https://swapi.dev/api/films/2/",
+      "https://swapi.dev/api/films/3/"
+    ],
+    "created": "2014-12-10T16:59:45.094000Z",
+    "edited": "2014-12-20T21:23:49.880000Z",
+    "url": "https://swapi.dev/api/starships/10/"
+  }
 
-  beforeEach(() => {
-    render(<Films starship={mockFn} />);
-  });
-
-  it("should render h2 with text Films", () => {
+  it("should render", () => {
+    render(<Films starship={starship} />)
     const h2 = screen.getByRole("heading", { level: 2 });
     expect(h2).toBeInTheDocument();
     expect(h2.textContent).toBe("Films");
