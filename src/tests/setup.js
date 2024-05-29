adde4d867085fdc1b176b45c7a628a7cd68be8e1
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-05-29 12:01:24 +0200

    solución mergeo setup

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,2 +1,8 @@
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+afterEach(() => {
+  //depues de cada prueba
+  cleanup(); //limpia el dom
+});
