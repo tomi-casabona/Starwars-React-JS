135a62305eaf22fa73fc8975074af7430d5f9bb6
Tomas <tomas.casabona@gmail.com>
2024-05-30 17:00:17 +0200

    firebase config was changed for enviroment

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -8,16 +8,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa-7Q4WzRXZQCtyMQDgzkHaPSJRYSVAWM",
-  authDomain: "prueba-tomi-casabona.firebaseapp.com",
-  projectId: "prueba-tomi-casabona",
-  storageBucket: "prueba-tomi-casabona.appspot.com",
-  messagingSenderId: "1009356613003",
-  appId: "1:1009356613003:web:328f926991e96a94d37b8e",
-  measurementId: "G-DY8D1YXQV9",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 export const appFirebase = initializeApp(firebaseConfig);
 export const analyticsFirebase = getAnalytics(appFirebase);
 export const auth = getAuth(appFirebase);
+
